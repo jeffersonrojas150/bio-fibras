3a141131355e92d6a8d58f08651079845a1ebfe6
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-10-21 23:38:48 -0500

    Mejora: mejora en el herobanner

--- frontend/src/components/sections/HeroBanner/HeroBanner.jsx
+++ frontend/src/components/sections/HeroBanner/HeroBanner.jsx
@@ -4,8 +4,8 @@ import './HeroBanner.css';
 
 // 1. Importa tus imágenes locales aquí
 import localImage from '../../../assets/herobiofibra.jpg'; 
-//import localImage2 from '../../../assets/herobanner2.jpg';
-//import localImage3 from '../../../assets/herobanner3.jpg'; 
+import localImage3 from '../../../assets/herobanner3.jpg';
+import localImage4 from '../../../assets/herobanner4.jpg';
 
 const HeroBanner = () => {
   const [currentSlide, setCurrentSlide] = React.useState(0);
@@ -13,7 +13,7 @@ const HeroBanner = () => {
   const carouselImages = [
     {
       id: 1,
-      src: 'https://scontent.flim28-1.fna.fbcdn.net/v/t39.30808-6/516433285_3988554351461014_7467849132525422307_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG2km5nyBV5VPqpodBIrv81HkKDP_pXOTkeQoM_-lc5OXWPt5mZUCkxAVpBdQRWE2LUmOH34C4-8rPeMz0JuEk_&_nc_ohc=QdLSVGzWpHIQ7kNvwGK6viH&_nc_oc=AdkSzfPQiwL0GboXOTlhsEL6GPR2V-MRA20AoxyI6a85MVk125SU74Tpc6zI8ErS_Bw&_nc_zt=23&_nc_ht=scontent.flim28-1.fna&_nc_gid=S3f8uz7Bx42AyM8cqXe1LA&oh=00_AfeeE5YfRqnAC_Wo3nJSu4WHKWaFGhQM14D4Rt_n8g0-ig&oe=68F79AAB', 
+      src: localImage4,
       alt: 'Artesanías de biofibras',
       category: 'ARTESANÍAS',
       title: 'Arte natural, hecho',
@@ -31,7 +31,7 @@ const HeroBanner = () => {
     },
     {
       id: 3,
-      src: 'https://scontent.flim28-1.fna.fbcdn.net/v/t51.82787-15/536164110_18072813638021521_8787962805984360695_n.jpg?stp=dst-jpegr_tt6&_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF2PHbX7oqD_UZjAXVlYi5LJkCJfzqzT00mQIl_OrNPTV9TU62ENVgMPHCPnr4wcTOH9HHSxhpAXep2aICB-CRo&_nc_ohc=CNWdG5GVIF8Q7kNvwEC4yaj&_nc_oc=AdmkH5j7Vj9pbmZJCa49L73muQIV0JFwZglck8BdO8J44q79URRMqvYcHePm1pgAVjs&_nc_zt=23&se=-1&_nc_ht=scontent.flim28-1.fna&_nc_gid=cHt71y_U3I_sdhBJ7NtYCQ&oh=00_AfdUzj5qmwxD5VvCeRVBXHnO_0WYTxtDjPllAofsTKix5g&oe=68F77987',  
+      src: localImage3,
       alt: 'Decoración natural',
       category: 'DECORACIÓN',
       title: 'Transforma tu espacio',
